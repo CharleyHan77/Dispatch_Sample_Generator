0a13d197aca46154c37671c3a800a7c884f31d2c
CharleyHan77 <disparity_han@163.com>
2025-03-06 13:28:56 +0800

    feat:initial add

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # Dispatch-Sample-Generator
 
 ## 1.项目描述
-
-- 基于fjsp场景下的历史`生产数据`，生成对应的`调度样本数据`。
-- 参考repo：https://github.com/CharleyHan77/Flexible-Job-Shop-Scheduling-Problem.git
+- 2026 毕设
+- 期望通过fjsp场景下的历史`生产数据`，生成推荐模型所需的对应`调度样本数据`。
 
 ## 2.使用说明
 
@@ -19,4 +18,9 @@
 - app 主要逻辑代码
 - dataset fjsp生产数据集
 - result 运行结果（一般为导出的excel，用于构成调度样本）
-- main.py
+- 
+
+## 4.参考repo
+- 基于框架repo：https://github.com/CharleyHan77/Flexible-Job-Shop-Scheduling-Problem.git
+- 涉及到的fjsp数据集：https://github.com/Lei-Kun/FJSP-benchmarks.git
+- 相关调度规则：https://github.com/Lei-Kun/Dispatching-rules-for-FJSP.git
--- test_SPT.py
+++ test_SPT.py
@@ -8,12 +8,6 @@ from app.customparser import parse
 import pandas as pd
 
 def start(path):
-    # for i in range(1, 19):
-    #     if i < 10:
-    #         i = "0" +str(i)
-    #     else:
-    #         i = str(i)
-    #path = f"data/Hurink_Data/Text/edata/{file_path}"
     print(path)
     jobs_list, machines_list, number_max_operations = parse(path)
     number_total_machines = len(machines_list)
@@ -49,20 +43,22 @@ def once_schedule(path, number_total_machines, number_total_jobs, number_max_ope
     stop = timeit.default_timer()
     return stop - start, makespan
 
-# python main.py
+# python test_SPT.py
 if __name__ == "__main__":
-    directory = "dataset/Hurink_Data/Text/vdata"
+    # 依次遍历不同数据集下的所有fjs文件
+    directory = "dataset/Brandimarte"
     file_path_list, makespan_list, time_list = list(), list(), list()
     for root, dirs, files in os.walk(directory):
         for file in files:
             # 获取文件的完整路径
             file_path = os.path.join(root, file)
             # 将文件名作为字符串传入脚本运行
-            #file_path = "data/Hurink_Data/Text/rdata\orb10.fjs"
-            mapkespan, time = start(file_path)
-            file_display = "/Text/vdata" + f"/{file}"
+            #file_path = "data/Hurink/Text/rdata\orb10.fjs"
+            makespan, time = start(file_path)
+            file_display = f"/Text/"
+            #file_display = file
             file_path_list.append(file_display)
-            makespan_list.append(mapkespan)
+            makespan_list.append(makespan)
             time_list.append(time)
     # 直接导出excel
     # file_path makespan time
@@ -78,5 +74,5 @@ if __name__ == "__main__":
     df = pd.DataFrame(data)
     columns_to_export = ['file_path', 'makespan', "time"]
     df_exported = df[columns_to_export]
-    output_file = 'result/temp_xls/Hurink_Data_vdata_MS_SPT_0218.xlsx'
+    output_file = 'result/temp_xls/Brandimarte_MS_SPT.xlsx'
     df.to_excel(output_file, index=False)
